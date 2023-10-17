72c2b143ba2fe0e6191e5c10c211388fc6d79651
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-17 18:31:55 +0200

    even-odd-set

--- js/pari-e-dispari.js
+++ js/pari-e-dispari.js
@@ -1,20 +1,21 @@
 
 let btn = document.querySelector('button')
-let evenOddPlayer = 'even';
 
 btn.addEventListener('click',
 function()
 {
     //numero player preso dal value dell'input radio
     let numberPlayer = document.querySelector('input[name="number_player"]:checked').value;
+    //even odd preso dal value dell'input radio
+    let evenOddPlayer = document.querySelector('input[name="even_odd"]:checked').value;
     // numero random computer
     let numberComputer = getRndInteger(1,5);
 
-    console.log(`Player ${numberPlayer}`)
-    console.log(`Computer ${numberComputer}`)
-    console.log(parseInt(numberPlayer) + numberComputer)
+    console.log(`Player ${numberPlayer}`);
+    console.log(`Computer ${numberComputer}`);
+    console.log(parseInt(numberPlayer) + numberComputer);
 
     let result = ((parseInt(numberPlayer) + numberComputer) % 2 === 0 && evenOddPlayer === 'even') ? 'Player win' : 'computer win';
     
-    console.log(result)
+    console.log(result);
 });
