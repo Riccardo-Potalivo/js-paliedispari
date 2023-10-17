c4c345b9c8ab19cd423f5274ef65139e1f7f99fb
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-17 15:51:48 +0200

    input-operation-set

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+const inputEl = document.getElementById('word');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click',
+function()
+{
+    console.log(inputEl.value);
+
+});
