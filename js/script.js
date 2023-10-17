8eb2dcad385b2e9b5ec325cc67b070bb4ef4fd1f
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-17 16:07:06 +0200

    reverse-set

--- js/script.js
+++ js/script.js
@@ -8,3 +8,11 @@ function()
     console.log(inputEl.value);
 
 });
+
+//generare ordine inverso della stringa
+let word = 'ciao';
+let reverse = '';
+for(let i = word.length - 1; i >= 0; i--)
+{
+    console.log(word[i])
+}
