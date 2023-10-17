1bb97dc6845b2bc7abb9cbf905ed6882f4231dc1
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-17 19:27:37 +0200

    exercise-done

--- js/pari-e-dispari.js
+++ js/pari-e-dispari.js
@@ -11,12 +11,28 @@ function()
     let evenOddPlayer = document.querySelector('input[name="even_odd"]:checked').value;
     // numero random computer
     let numberComputer = getRndInteger(1,5);
-
+    
+    alert.classList.remove('bg-warning');
+    
     console.log(`Player ${numberPlayer}`);
     console.log(`Computer ${numberComputer}`);
     console.log(parseInt(numberPlayer) + numberComputer);
 
-    let result = ((parseInt(numberPlayer) + numberComputer) % 2 === 0 && evenOddPlayer === 'even' || (parseInt(numberPlayer) + numberComputer) % 2 === 1 && evenOddPlayer === 'odd') ? 'Player win' : 'computer win';
+    // let result = ((parseInt(numberPlayer) + numberComputer) % 2 === 0 && evenOddPlayer === 'even' || (parseInt(numberPlayer) + numberComputer) % 2 === 1 && evenOddPlayer === 'odd') ? 'Player win' : 'computer win';
+
+
+    if((parseInt(numberPlayer) + numberComputer) % 2 === 0 && evenOddPlayer === 'even' || (parseInt(numberPlayer) + numberComputer) % 2 === 1 && evenOddPlayer === 'odd')
+    {
+        alert.innerText = 'Player win';
+        alert.classList.add('bg-success');
+        alert.classList.remove('bg-danger');
+    }
+    else
+    {
+        alert.innerText = 'Computer win';
+        alert.classList.add('bg-danger');
+        alert.classList.remove('bg-success');
+    }
     
-    console.log(result);
+    // console.log(result);
 });
