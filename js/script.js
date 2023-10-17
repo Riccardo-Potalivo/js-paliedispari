c1059fe879856e4192df19469dce8513c138b22c
Riccardo Potalivo <riccardopotalivo@gmail.com>
2023-10-17 16:19:25 +0200

    Palindroma-done

--- js/script.js
+++ js/script.js
@@ -5,14 +5,17 @@ const btn = document.querySelector('button');
 btn.addEventListener('click',
 function()
 {
-    console.log(inputEl.value);
-
+    let word = inputEl.value;
+    let reverse = '';
+    for(let i = word.length - 1; i >= 0; i--)
+    {
+        console.log(word[i]);
+        reverse += word[i];
+    }
+    console.log(word)
+    console.log(reverse)
+    let result = (word === reverse) ? 'palindromo' : 'non palindromo';
+    console.log(result) 
 });
 
 //generare ordine inverso della stringa
-let word = 'ciao';
-let reverse = '';
-for(let i = word.length - 1; i >= 0; i--)
-{
-    console.log(word[i])
-}
